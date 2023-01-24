b712fdee97a4a92e502e10a7b180c63ace90d6ea
regis1425 <regianemarcelino81@gmail.com>
2023-01-24 15:15:09 -0300

    primeira alteração

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            Regiane Estevão Rosa Marcelino
+          
           </p>
           <div>
             <a
